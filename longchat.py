2c00b878f938b3372fcbad07daed64d19c7748c6
Jarno Rantaharju <jarno.rantaharju@aalto.fi>
2023-06-26 23:22:14 +0300

    Pickle the memory to avoid repeated queries

--- longchat.py
+++ longchat.py
@@ -5,6 +5,7 @@ import os
 import functions
 from Levenshtein import distance
 import traceback
+import pickle
 
 from langchain.document_loaders import TextLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -16,7 +17,6 @@ with open('api_key', 'r') as file1:
     openai.api_key = file1.readlines()[0].strip()
 
 os.environ['OPENAI_API_KEY'] = openai.api_key
-
 
 
 
@@ -89,36 +89,64 @@ Your reply must only contain notes following this syntax, and no other text.
         if "memory_file" in content.keys():
             self.memory_file = content["memory_file"]
         else:
-            self.memory_file = self.conversation.replace(".json", "_memory.json")
+            self.memory_file = self.conversation.replace(".json", "_memory.pickle")
 
+    def split_message(self, message, max_split_len = 200):
+        # split the message
+        splits = message["content"].split("\n")
+        splits = [s for s in splits if len(s) > 0]
 
-    def encode_conversation(self, max_split_len = 200):
-        texts = []
-        import re
-        for message in self.messages:
-            # split the message
-            splits = message["content"].split("\n")
-            splits = [s for s in splits if len(s) > 0]
-
-            #combine small chunks, keeping below max_split_len
-            combined_splits = []
-            current_split = ""
-            for split in splits:
-                if len(split) > 2*max_split_len:
-                    print("long split", len(split))
-                if len(current_split) + len(split) <= max_split_len:
-                    if current_split:
-                        current_split += "\n" + split
-                    else:
-                        current_split = split
+        #combine small chunks, keeping below max_split_len
+        combined_splits = []
+        current_split = ""
+        for split in splits:
+            if len(split) > 2*max_split_len:
+                print("long split", len(split))
+            if len(current_split) + len(split) <= max_split_len:
+                if current_split:
+                    current_split += "\n" + split
                 else:
-                    if current_split:
-                        combined_splits.append(current_split)
                     current_split = split
-            if current_split:
-                combined_splits.append(current_split)
-            texts += splits
-        self.db = FAISS.from_texts(texts, OpenAIEmbeddings())
+            else:
+                if current_split:
+                    combined_splits.append(current_split)
+                current_split = split
+        if current_split:
+            combined_splits.append(current_split)
+        return combined_splits
+
+    def encode_new_messages(self):
+        try:
+            with open(self.memory_file, "rb") as file:
+                self.db = pickle.load(file)
+
+            texts = []
+            for message in self.messages[-2:]:
+                texts += self.split_message(message)
+            self.db.add_texts(texts)
+        except:
+            texts = []
+            for message in self.messages:
+                texts += self.split_message(message)
+            self.db = FAISS.from_texts(texts, OpenAIEmbeddings())
+
+        with open(self.memory_file, "wb") as file:
+            pickle.dump(self.db, file)
+
+    def encode_conversation(self):
+        try:
+            with open(self.memory_file, "rb") as file:
+                self.db = pickle.load(file)
+            print("LOADED")
+        except:
+            texts = []
+            for message in self.messages:
+                texts += self.split_message(message)
+            self.db = FAISS.from_texts(texts, OpenAIEmbeddings())
+
+            with open(self.memory_file, "wb") as file:
+                pickle.dump(self.db, file)
+
 
     def build_notes_message(self, messages, max_notes = 5):
         self.encode_conversation()
@@ -212,7 +240,7 @@ Your reply must only contain notes following this syntax, and no other text.
             }, outfile, indent=4)
 
     def check_summary(self):
-        self.encode_conversation()
+        self.encode_new_messages()
 
         if (self.messages_since_summary >= self.summarize_every) or self.first_summary:
             self.summarize_chatgpt()
